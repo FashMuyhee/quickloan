ffdbaaf3f3a4c189053d530be7c7bcd3b082ae0f
unknown <fashmuyidavid@gmail.com>
2020-12-21 21:47:41 +0100

    new components

--- app/components/index.js
+++ app/components/index.js
@@ -1,4 +1,7 @@
 import Container from './Container';
 import Welcome from './Welcome';
+import ScrollContainer from './ScrollContainer';
+import TextButton from './TextButton';
+import MyCard from './MyCard';
 
-export {Container, Welcome};
+export {Container, Welcome, ScrollContainer, TextButton,MyCard};
